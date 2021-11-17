ea27d17ff74be969a9c287d0ba64e2bd1fd68917
Jedrzej Szadejko <shadyprywatny@gmail.com>
2021-11-17 13:47:16 +0100

    Cleanup

--- exercise_1.py
+++ exercise_1.py
@@ -22,37 +22,39 @@ def exercise_one():
             )
     lambda_rate = calculate_reliability_lambda(reliability_rate, time)
     reliability_for_time = calculate_reliability_for_time(lambda_rate, time_target)
-    print(reliability_for_time)
+    print("Below are preset values:")
     print("exc1.1 ", calculate_reliability_for_time(calculate_reliability_lambda(97, 100), 200))
     print("exc1.2 ", calculate_reliability_for_time(calculate_reliability_lambda(97, 100), 100))
+    print("Below are your values:")
+    print(f"Result: {reliability_for_time}")
 
 def exercise_two(failure_rate, quantity, days):
     # Hourly failure rate used to calculate annual failure rate
     # This should be discussed as I'm not sure whether I understood the assignment
     reliability = calculate_reliability_for_time(failure_rate, days)
-    print(f'Approx. {quantity-(quantity*reliability)} devices will fail')
+    print(f'Exc. 2 Approx. {quantity-(quantity*reliability)} devices will fail')
 
-def exercise_three():
-    reliability = math.exp(-300/10**-4)
-    print(reliability)
+def exercise_three(initial_time_without_failure, target_time):
+    reliability = math.exp(-initial_time_without_failure/(target_time * 24))
+    print("Exc. 3 Probability: ", reliability)
 
 def exercise_four(expected_reliability, time):
     failure_rate = calculate_reliability_lambda(expected_reliability, time)
-    print(f'Maximum failure rate is: {failure_rate}, MTBF is: {1/failure_rate}')
+    print(f'Exc. 4 Maximum failure rate is: {failure_rate}, MTBF is: {1/failure_rate}')
 
 def exercise_five(failure_rate, expected_return_rate):
     # This might be bad math
     days_for_expected_rate = math.log(100-expected_return_rate)/-failure_rate
-    print(1/days_for_expected_rate*365)
+    print(f'Exc. 5 {1/days_for_expected_rate*365}')
 
 
 def main():
-    #exercise_three()
+    exercise_one()
     exercise_two(0.5*10**-7, 5000, 8766)
     exercise_two(2.5*10**-7, 5000, 8766)
+    exercise_three(100, 300)
     exercise_four(95, 500)
     exercise_five(0.4, 5)
-    #exercise_two(1, 2)
 
 if __name__ == "__main__":
     main()
